c0d2be9d03333dfd1c125a5db88af80741f3cc12
Bafodé <bafode.cisse@epitech.eu>
2024-07-21 16:56:50 +0200

    (Edit) : Editing products list with cards

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,11 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './Products.css';
 
-export default function Home()
-{
+const Products = () => {
+  const [products, setProducts] = useState([]);
 
-return(
-    <div>
-    <h1>Hello World !</h1>
+  useEffect(() => {
+    fetch('http://localhost:8000/api/products')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data.produits);
+      });
+  }, []);
+
+  return (
+    <div className="products-container">
+      <h1>Products</h1>
+      <div className="products-grid">
+        {products.map((product) => (
+          <div className="product-card" key={product.id}>
+            <img src={product.image} alt={product.name} className="product-image" />
+            <div className="product-details">
+              <h2 className="product-name">{product.name}</h2>
+              <p className="product-price">Prix : {product.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-);
+  );
+};
 
-}
+export default Products;
