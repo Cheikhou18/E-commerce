4f38aee4e719e1ac970cce0916cc33a3a4ac0869
Max <chenmax02@outlook.com>
2024-07-22 15:54:57 +0200

    (Add): Signup functionality

--- server/src/Controller/AuthenticationController.php
+++ server/src/Controller/AuthenticationController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use DateTime;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,19 +15,17 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class AuthenticationController extends AbstractController
 {
     private EntityManagerInterface $em;
-    private UserPasswordHasherInterface $passwordHasher;
 
-    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
+    public function __construct(EntityManagerInterface $em)
     {
         $this->em = $em;
-        $this->passwordHasher = $passwordHasher;
     }
 
     #[Route('/api/signup', name: 'signup')]
     public function signup(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $data = json_decode($request->getContent(), false);
-        $userExists = $this->em->getRepository(User::class)->findOneByMail($data->mail);
+        $userExists = $this->em->getRepository(User::class)->findOneByEmail($data->email);
 
         if ($userExists) {
             return $this->json(['success' => false, 'message' => 'User already exists']);
@@ -34,23 +34,26 @@ class AuthenticationController extends AbstractController
         $plainPassword = $data->password;
 
         $user = new User();
-        $user->setFirstname($data->firstname);
-        $user->setLastname($data->lastname);
-        $user->setMail($data->email);
+        $user->setEmail($data->email);
 
         $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
         $user->setPassword($hashedPassword);
+        $user->setCreationDate(new DateTimeImmutable());
+
+        $this->em->persist($user);
+        $this->em->flush();
 
         return $this->json([
             'success' => true,
+            'user' => $user
         ]);
     }
 
     #[Route('/api/signin', name: 'signin')]
-    public function signin(Request $request): JsonResponse
+    public function signin(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $data = json_decode($request->getContent(), false);
-        $userExists = $this->em->getRepository(User::class)->findOneByMail($data->email);
+        $userExists = $this->em->getRepository(User::class)->findOneByEmail($data->email);
 
         if (!$userExists) {
             return $this->json(['success' => false, 'message' => 'User does not exist']);
