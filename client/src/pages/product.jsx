a9e61aaadc6490c67d9edf3436c9ab3e21ba82af
Max <chenmax02@outlook.com>
2024-07-24 14:48:32 +0200

    (Fix): Fetch products details

--- client/src/pages/product.jsx
+++ client/src/pages/product.jsx
@@ -1,20 +1,24 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getProductDetails } from "../api/productsDetails";
+import { getProductById } from "../api/products";
 
 function Product() {
   const { id } = useParams();
   const [products, setProducts] = useState();
-  
+
   useEffect(() => {
     async function fetchDetails() {
-        setProducts(await getProductDetails());
+      const request = await getProductById(id);
+
+      if (request.success) {
+        setProducts(await getProductById(id));
+      }
     }
 
     fetchDetails();
-  }, [])
+  }, []);
 
-  console.log(id);
+  console.log(products);
 }
 
 export default Product;
--- server/src/Controller/ProductController.php
+++ server/src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
@@ -21,11 +22,26 @@ class ProductController extends AbstractController
     public function index()
     {
         $products = $this->entityManager->getRepository(Product::class)->getAll();
-        // dd($products);
 
         return $this->json([
-            'success' => true,  
+            'success' => true,
             'produits' => $products
+        ]);
+    }
+
+    #[Route('/api/products/{id}', name: 'app_product_details')]
+    public function productdetails(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $id = $request->get('id');
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            return $this->json(['success' => false]);
+        }
+
+        return $this->json([
+            'success' => true,
+            'product' => $product,
         ]);
     }
 }
