b5f6ada41724a0a85f25c7e2966bf7c65e0b48ce
Max <chenmax02@outlook.com>
2024-07-26 17:19:27 +0200

    (Add): Categories CRUD

--- client/src/pages/admin.jsx
+++ client/src/pages/admin.jsx
@@ -1,31 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/admin";
-import Navbar from "../components/navbar";
 import {
   addProduct,
   deleteProduct,
   editProduct,
   getProducts,
 } from "../api/products.js";
+import { createCategory } from "../api/auth/admin.js";
 
 const Admin = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState();
+  const [newCategory, setNewCategory] = useState();
+  const [message, setMessage] = useState({ categories: "", products: "" });
+
   const navigate = useNavigate();
   const { isAdmin } = useAuth() || {};
 
   useEffect(() => {
     if (isAdmin === false) return navigate("/");
-
     fetchProducts();
-  }, []);
+  }, [isAdmin]);
 
   async function fetchProducts() {
     const request = await getProducts();
-
-    if (request.success) {
-      setProducts(request.produits);
-    }
+    if (request.success) setProducts(request.produits);
   }
 
   const handleAddProduct = async () => {
@@ -68,27 +67,56 @@ const Admin = () => {
     }
   };
 
+  async function handleCategoryForm(e) {
+    e.preventDefault();
+
+    const request = await createCategory(newCategory);
+
+    setMessage(request.message);
+
+    // Clears the message after 2 seconds
+    setTimeout(() => setMessage({ ...message, categories: "" }), 2 * 1000);
+  }
+
   return (
-    <div>
-      <Navbar/>
-      <h1>Products</h1>
+    <div className="flex flex-col gap-6">
+      <h3>Categories</h3>
+
+      <form onSubmit={(e) => handleCategoryForm(e)}>
+        <div className="flex flex-col w-fit">
+          <label>Create a category</label>
+          <input
+            type="text"
+            placeholder="Category name..."
+            onChange={(e) => setNewCategory(e.target.value)}
+          />
+        </div>
+
+        {message.categories}
+
+        <button>Create category</button>
+      </form>
+
+      <h3>Products</h3>
 
       <button onClick={handleAddProduct}>Add Product</button>
 
       <ul>
         {products?.map((product) => (
-          <li key={product.id}>
-            {product.name} - {product.price}€
-            {isAdmin && (
-              <>
-                <button onClick={() => handleEditProduct(product.id)}>
-                  Edit
-                </button>
-                <button onClick={() => handleDeleteProduct(product.id)}>
-                  Delete
-                </button>
-              </>
-            )}
+          <li className="border-t p-4" key={product.id}>
+            <p>
+              {product.name} - {product.price}€
+            </p>
+
+            <div>
+              <button onClick={() => handleEditProduct(product.id)}>
+                Edit
+              </button>
+
+              <button onClick={() => handleDeleteProduct(product.id)}>
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
