e2060b4803bc8f7b5a61050a5d5eff5a5e7be2d7
Max <chenmax02@outlook.com>
2024-07-30 13:07:47 +0200

    (Add): Cart

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
 import "../assets/css/Products.css";
+import { useCartContext } from "../context/cart";
 
-function Navbar(){
-    return(
-       <div className="navbar">
-            <Link to={"/"}>Home</Link>
-            <Link to={"/products"}>All glasses</Link>
-            <Link to={"/signin"}>Sign in</Link>
-            <Link to={"/signup"}>Sign up</Link>
-            <Link to={"/admin"}>Admin</Link>
+function Navbar() {
+  const { changeViewCart, cartQuantity } = useCartContext();
 
-        </div>
-
-        
-    );
+  return (
+    <div className="navbar">
+      <Link to={"/"}>Home</Link>
+      <Link to={"/products"}>All glasses</Link>
+      <Link to={"/signin"}>Sign in</Link>
+      <Link to={"/signup"}>Sign up</Link>
+      <Link to={"/admin"}>Admin</Link>
+      <button onClick={() => changeViewCart()}>Cart</button>
+      {cartQuantity > 0 && <span>{cartQuantity}</span>}
+    </div>
+  );
 }
 export default Navbar;
---
+++ client/src/context/cart/index.js
@@ -0,0 +1,95 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { post } from "../../api";
+
+// Context of the shopping cart, it contains a context provider and a useContext
+// With the functions you should be able to get the products quantities in the cart,
+// add / remove a product and see the total price of the cart
+import { useLocalStorage } from "./hooks/localStorage";
+import { getProducts } from "../../api/products";
+
+const CartContext = createContext({});
+
+export function useCartContext() {
+  return useContext(CartContext);
+}
+
+function CartProvider({ children }) {
+  const [cartProducts, setCartProducts] = useLocalStorage("shopping_cart", []);
+  const [productsInDB, setProductsInDB] = useState([]);
+  const [viewCart, setViewCart] = useState(false);
+
+  useEffect(() => {
+    async function fetchProductsFromDB() {
+      const request = await getProducts();
+      if (request.success) setProductsInDB(request.produits);
+    }
+    fetchProductsFromDB();
+  }, [cartProducts]);
+
+  const cartQuantity = cartProducts.reduce(
+    (quantity, product) => product.quantity + quantity,
+    -2
+  );
+
+  function getProductQuantity(id) {
+    return cartProducts.find((product) => product.id === id)?.quantity || 0;
+  }
+
+  function increaseProductQuantity(id) {
+    setCartProducts((currProducts) => {
+      if (currProducts.find((product) => product.id === id) == null) {
+        return [...currProducts, { id, quantity: 1 }];
+      }
+
+      return currProducts.map((product) => {
+        if (product.id === id) {
+          return { ...product, quantity: product.quantity + 1 };
+        }
+
+        return product;
+      });
+    });
+  }
+
+  function decreaseProductQuantity(id) {
+    setCartProducts((currProducts) => {
+      if (currProducts.find((product) => product.id === id)?.quantity === 1) {
+        return currProducts.filter((item) => item.id !== id);
+      }
+
+      return currProducts.map((product) => {
+        if (product.id === id) {
+          return { ...product, quantity: product.quantity - 1 };
+        }
+
+        return product;
+      });
+    });
+  }
+
+  function removeFromCart(id) {
+    setCartProducts((currProduct) => {
+      return currProduct.filter((product) => product.id !== id);
+    });
+  }
+
+  function changeViewCart() {
+    return setViewCart(!viewCart);
+  }
+
+  const values = {
+    productsInDB,
+    cartProducts,
+    cartQuantity,
+    getProductQuantity,
+    increaseProductQuantity,
+    decreaseProductQuantity,
+    removeFromCart,
+    viewCart,
+    changeViewCart,
+  };
+
+  return <CartContext.Provider value={values}>{children}</CartContext.Provider>;
+}
+
+export default CartProvider;
