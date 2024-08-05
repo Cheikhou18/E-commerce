18cc8914b7d2673d368dc816864a6e987b58abcd
cheikhou <cheikhou.diakhaby@epitech.eu>
2024-08-05 18:05:45 +0200

    (Add): recommended

---
+++ client/src/components/RecommendedBadge.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/css/RecommendedBadge.css'; 
+
+const RecommendedBadge = () => {
+  return (
+    <span className="recommended-badge">Recommandé</span>
+  );
+};
+
+export default RecommendedBadge;
--- client/src/components/productCard.jsx
+++ client/src/components/productCard.jsx
@@ -1,20 +1,22 @@
+import React from 'react';
 import { useCartContext } from "../context/cart";
-
 import { Link } from "react-router-dom";
 import "../assets/css/Products.css";
+import RecommendedBadge from "./RecommendedBadge"; // Importez le nouveau composant
 
 function ProductCard(props) {
   const { product } = props;
   const { increaseProductQuantity } = useCartContext();
 
   return (
-    <div className="flex flex-col">
+    <div className="relative flex flex-col">
       <Link to={"/products/" + product.id}>
         <img src={product.image} alt={product.name} className="product-image" />
         <div className="product-details">
           <h2 className="product-name">{product.name}</h2>
           <p className="product-price">Price : {product.price}</p>
         </div>
+        {product.recommended && <RecommendedBadge />} {/* Affiche le badge si recommandé */}
       </Link>
 
       <button
