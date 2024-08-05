dacb2a0aa06a9d480cc4f2f122459c64439cadb4
Bafodé Cissé <bafode.cisse@epitech.eu>
2024-08-05 17:17:17 +0200

    (Add) : Adding a function to products.js

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -11,6 +11,9 @@ export async function getProductsByPopularity() {
 export async function getProductById(id) {
   return await get("/api/products/" + id);
 }
+export async function getProductByName(name) {
+  return await get("/api/products/" + name);
+}
 
 export async function addProduct(data) {
   return await post("/api/products", data);
---
+++ client/src/components/productColor.jsx
@@ -0,0 +1,6 @@
+import { useEffect,useState } from "react";
+
+function Color(props){
+    console.log(props);
+}
+export default Color;
