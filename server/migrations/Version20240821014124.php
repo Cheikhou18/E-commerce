c2d7130c97d6fe0276acdf6b97340086c1350d36
Max <chenmax02@outlook.com>
2024-08-21 03:45:33 +0200

    (Add): Delivery table

--- server/migrations/Version20240821014124.php
+++ server/migrations/Version20240821014124.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240819201717 extends AbstractMigration
+final class Version20240821014124 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,6 +21,7 @@ final class Version20240819201717 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE delivery (id INT AUTO_INCREMENT NOT NULL, delivery_type VARCHAR(30) NOT NULL, price_per_km DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE features (id INT AUTO_INCREMENT NOT NULL, features JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, id_user INT NOT NULL, order_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, id_features INT DEFAULT NULL, id_category INT NOT NULL, name VARCHAR(60) NOT NULL, image LONGTEXT NOT NULL, description LONGTEXT NOT NULL, price INT NOT NULL, popularity INT NOT NULL, stock INT NOT NULL, recommended TINYINT(1) NOT NULL, color VARCHAR(255) NOT NULL, discount INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -31,6 +32,7 @@ final class Version20240819201717 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE delivery');
         $this->addSql('DROP TABLE features');
         $this->addSql('DROP TABLE `order`');
         $this->addSql('DROP TABLE product');
