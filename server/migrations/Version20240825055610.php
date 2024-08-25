93c954305ee11bafc0fc472b90caf365f827a0ed
Bafodé Cissé <bafode.cisse@epitech.eu>
2024-08-25 08:04:21 +0200

    (Fix) :  Fix table orderS

--- server/migrations/Version20240825055610.php
+++ server/migrations/Version20240825055610.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240825054630 extends AbstractMigration
+final class Version20240825055610 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20240825054630 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD gift_wrapping SMALLINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE `order` ADD gift_wrapping SMALLINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE product DROP gift_wrapping');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP gift_wrapping');
+        $this->addSql('ALTER TABLE product ADD gift_wrapping SMALLINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE `order` DROP gift_wrapping');
     }
 }
--- server/src/Entity/Order.php
+++ server/src/Entity/Order.php
@@ -3,10 +3,11 @@
 namespace App\Entity;
 
 use App\Repository\OrderRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: OrderRepository::class)]
-#[ORM\Table(name: '`order`')]
+#[ORM\Table(name: '`orders`')]
 class Order
 {
     #[ORM\Id]
@@ -17,8 +18,11 @@ class Order
     #[ORM\Column]
     private ?int $id_user = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $order_date = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $order_date = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $gift_wrapping = null;
 
     public function getId(): ?int
     {
@@ -37,14 +41,26 @@ class Order
         return $this;
     }
 
-    public function getOrderDate(): ?\DateTimeImmutable
+    public function getOrderDate(): ?\DateTimeInterface
     {
         return $this->order_date;
     }
 
-    public function setOrderDate(\DateTimeImmutable $order_date): static
+    public function setOrderDate(\DateTimeInterface $order_date): static
     {
         $this->order_date = $order_date;
+
+        return $this;
+    }
+
+    public function getGiftWrapping(): ?int
+    {
+        return $this->gift_wrapping;
+    }
+
+    public function setGiftWrapping(?int $gift_wrapping): static
+    {
+        $this->gift_wrapping = $gift_wrapping;
 
         return $this;
     }
